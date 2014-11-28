adfb24586dc1f1f08750b1e0013be59874ae946e
wyongch <wyongch@gmail.com>
2014-11-28 15:42:03 +0800

    AsyncTask

--- src/com/example/asynctask/MainActivity.java
+++ src/com/example/asynctask/MainActivity.java
@@ -1,27 +1,55 @@
 package com.example.asynctask;
 
+import java.io.IOException;
+import java.net.URL;
+
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.os.Build;
 
 public class MainActivity extends ActionBarActivity {
 
+	ImageView mImageView ;
+	Button button ;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.fragment_main);
 
-		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
+		
+		mImageView = (ImageView)findViewById(R.id.imageview1) ;
+		button=(Button)findViewById(R.id.button1) ;
+		button.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				new DownloadImageTask().execute("http://img0.bdstatic.com/img/image/6446027056db8afa73b23eaf953dadde1410240902.jpg") ;
+			}
+		});
+//		button.setOnClickListener(new OnClickListener() {
+//			
+//			@Override
+//			public void onClick(View v) {
+//				// TODO Auto-generated method stub
+////				new DownloadImageTask().execute("http://img0.bdstatic.com/img/image/6446027056db8afa73b23eaf953dadde1410240902.jpg") ;
+//			}
+//		}); 
+		
 	}
 
 	@Override
@@ -57,8 +85,42 @@ public class MainActivity extends ActionBarActivity {
 				Bundle savedInstanceState) {
 			View rootView = inflater.inflate(R.layout.fragment_main, container,
 					false);
+//			rootView.findViewById(R.id.button1) ;
 			return rootView;
 		}
+	}
+	
+	public class DownloadImageTask extends AsyncTask<String, Void, Drawable> {
+
+		@Override
+		protected Drawable doInBackground(String... urls) {
+			// TODO Auto-generated method stub
+			return loadImageFromNetwork(urls[0]);
+		}
+		
+		private Drawable loadImageFromNetwork(String imageUrl) {
+			// TODO Auto-generated method stub
+			Drawable drawable = null;  
+		    try {  
+		        // 可以在这里通过文件名来判断，是否本地有此图片  
+		        drawable = Drawable.createFromStream(  
+		                new URL(imageUrl).openStream(), "image.jpg");  
+		    } catch (IOException e) {  
+		        Log.d("test", e.getMessage());  
+		    }  
+		    if (drawable == null) {  
+		        Log.d("test", "null drawable");  
+		    } else {  
+		        Log.d("test", "not null drawable");  
+		    }  
+		      
+		    return drawable ;  
+		}
+
+		protected void onPostExecute(Drawable result){
+			 mImageView.setImageDrawable(result);
+		}
+		
 	}
 
 }
